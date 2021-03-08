f6a92d904250b73c9be564af6b14049f2ba54fd1
yamuna199 <74089739+yamuna199@users.noreply.github.com>
2021-03-08 19:05:40 +0530

    Add files via upload

---
+++ deployment-master/src/main/java/com/cg/DeploymentSample/App.java
@@ -0,0 +1,20 @@
+package com.cg.DeploymentSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ ***
+ ***
+ ***
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+       System.out.println("Welcome bindu garu");
+    }
+}
